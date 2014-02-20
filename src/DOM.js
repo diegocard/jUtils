d245a9b9d80f5e099af3a7201cce9f5e6160a646
Diego Cardozo <diegocard@gmail.com>
2014-02-20 16:08:07 -0200

    Added documentation and method chaining

--- src/DOM.js
+++ src/DOM.js
@@ -2,6 +2,9 @@
 
 /* global JSUtils:true, XMLHttpRequest:false */
 
+/*
+ * RESTRICTION: IE9+
+ */
 JSUtils.getJSON = function(url, success, error) {
   // TODO: Doc, test, add extra parameters
   var request = new XMLHttpRequest(),
@@ -24,13 +27,22 @@ JSUtils.getJSON = function(url, success, error) {
   };
 
   request.send();
+  return JSUtils;
 };
 
+
+/*
+ * RESTRICTION: IE8+
+ */
 JSUtils.ajaxPost = function(url, data) {
   // TODO: Doc, test, add extra parameters, check if success and error functions can be added as parameters
   var request = new XMLHttpRequest();
   request.open('POST', url, true);
   request.send(data);
+  return JSUtils;
 };
 
+
+// TODO: Check if method chaining works, add to other methods
 // TODO: Combine getJSON, ajaxPost and ajaxGet into a single ajax function?
+// TODO: Create a full element selector engine?
